2b4826faaf6927671a72478b99a32d312d8b8849
Vincent Castelluci <vincent.castelluci@gmail.com>
2022-05-14 01:38:47 +0200

    feat: Implement auction

--- 2-oracles/auction_house/scrypto/auction/src/lib.rs
+++ 2-oracles/auction_house/scrypto/auction/src/lib.rs
@@ -1,40 +1,153 @@
 use scrypto::prelude::*;
 
+#[derive(NonFungibleData)]
+struct ParticipantData {
+    id: u128,
+}
+
 blueprint! {
     struct Auction {
-        admin_badge_def: ResourceAddress,
-        uuid: u128,
-        badges: Bucket,
-        initial_price: Decimal,
-        bin_price: Decimal
+        id: u128,
+        current_bid: Decimal,
+        minimal_bid: Decimal,
+        bin_price: Decimal,
+        ended: bool,
+        product: Vault,
+
+        gathered: Vault,
+        bids: HashMap<NonFungibleId, Vault>,
+
+        // Owner
+        owner_badge_ref: ResourceAddress,
+
+        // Participant
+        participant_minter: Vault,
+        participant_nft_address: ResourceAddress,
     }
 
     impl Auction {
-        // given a price in XRD, creates a ready-to-use gumball machine
-        pub fn instantiate_gumball_machine(initial_price: Decimal, bin_price: Decimal) -> Auction {
+        pub fn new(minimal_bid: Decimal, bin_price: Decimal, product: Bucket) -> (ComponentAddress, Bucket) {
 
-            let uuid = Runtime::generate_uuid();
+            // Auction relative
+            let id = Runtime::generate_uuid();
+            let product: Vault = Vault::with_bucket(product);
 
-            // Create the admin badges
-        let badges: Bucket = ResourceBuilder::new_fungible().divisibility(DIVISIBILITY_NONE)
-        .metadata("name", format!("Auction {} owner", uuid.into()))
-        .initial_supply(dec!(1));
+            // Owner relative
+            let owner_badge: Bucket = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .metadata("name", format!("Owner of {} auction", id.to_string()))
+                .initial_supply(1);
+
+            // Participant relative
+            let participant_minter: Bucket = ResourceBuilder::new_fungible()
+            .divisibility(DIVISIBILITY_NONE)
+            .initial_supply(1);
+
+            let participant_nft_address: ResourceAddress = ResourceBuilder::new_non_fungible()
+            .metadata("name", format!("Participant of {} auction", id.to_string()))
+            .mintable(rule!(require(participant_minter.resource_address())), LOCKED)
+            .burnable(rule!(require(participant_minter.resource_address())), LOCKED)
+            .no_initial_supply();
 
             let component = Self {
-                admin_badge_def: badges.resource_address(),
-                uuid,
-                badges,
-                initial_price,
+                id,
+                current_bid: dec!(0),
+                minimal_bid,
                 bin_price,
+                ended: false,
+                product,
+                gathered: Vault::new(RADIX_TOKEN),
+                bids: HashMap::new(),
+                owner_badge_ref: owner_badge.resource_address(),
+                participant_minter: Vault::with_bucket(participant_minter),
+                participant_nft_address,
             }.instantiate();
 
-            // Define the access rules for this blueprint.
+            // Access control
             let access_rules = AccessRules::new()
-                .method("do_admin_task", rule!(require(badges.resource_address())));
+                .method("withdraw", rule!(require(owner_badge.resource_address())))
+                .method("bid", rule!(require(participant_nft_address)))
+                .default(AccessRule::AllowAll);
 
-            // Return the component and the badges
-            (component.add_access_check(access_rules).globalize(), badges)
+            // Component with owner badge
+            (component.add_access_check(access_rules).globalize(), owner_badge)
+        }
 
+        pub fn register(&mut self) -> Bucket {
+            let badge = self.participant_minter.authorize(|| {
+                let participant_nft_manager: &ResourceManager = borrow_resource_manager!(self.participant_nft_address);
+                participant_nft_manager.mint_non_fungible(&NonFungibleId::random(), ParticipantData{id: Runtime::generate_uuid()})
+            });
+
+            self.bids.insert(badge.non_fungible::<ParticipantData>().id(), Vault::new(RADIX_TOKEN));
+
+            badge
+        }
+
+        pub fn bid(&mut self, amount: Bucket, participant_badge: Proof) -> (Bucket, Bucket) {
+            assert!(self.bin_price > self.current_bid, "Auction has ended, BIN price was reached.");
+            assert!(!self.ended, "Auction has ended.");
+
+            let id = participant_badge.non_fungible::<ParticipantData>().id();
+
+            assert!(self.bids.contains_key(&id), "You need to register to participate auction first.");
+
+            let already_bidded = self.bids[&id].amount();
+            let additional_bid = amount.amount();
+            let total_bid = already_bidded + additional_bid;
+
+            assert!(total_bid >= self.minimal_bid, "Minimal bid is {}, your total bid would be {}.", self.minimal_bid, total_bid);
+            assert!(total_bid > self.current_bid, "Current bid is {}, your total bid would be {}.", self.current_bid, total_bid);
+
+            if total_bid >= self.bin_price {
+                self.ended = true;
+                self.current_bid = self.bin_price;
+                let overflow = total_bid - self.bin_price;
+                let left_overs = self.bids.get_mut(&id).unwrap().take(overflow);
+                self.gathered.put(self.bids.get_mut(&id).unwrap().take_all());
+                self.gathered.put(amount);
+
+                return (left_overs, self.product.take_all())
+            }
+
+            self.current_bid = total_bid;
+
+            self.bids.get_mut(&id).unwrap().put(amount);
+
+            (self.bids.get_mut(&id).unwrap().take(0), self.product.take(0))
+        }
+
+        pub fn withdraw(&mut self) -> Bucket {
+            self.gathered.take_all()
+        }
+
+        pub fn withdraw_outbidded(&mut self, participant_badge: Proof) -> Bucket {
+            let id = participant_badge.non_fungible::<ParticipantData>().id();
+
+            assert!(self.bids.contains_key(&id), "You are not registered to the auction, you were never outbidded.");
+
+            let already_bidded = self.bids[&id].amount();
+
+            assert!(already_bidded != self.current_bid, "You are holding the current bid, you were never outbidded.");
+
+            self.bids.get_mut(&id).unwrap().take_all()
+        }
+
+        pub fn unregister(&mut self, participant_bucket: Bucket) {
+            assert!(self.ended, "Auction has not yet ended, you can not unregister.");
+
+            let id = participant_bucket.non_fungible::<ParticipantData>().id();
+
+            assert!(self.bids.contains_key(&id), "You are not registered to the auction, you do not need to unregister.");
+
+            let already_bidded = self.bids[&id].amount();
+
+            assert!(already_bidded == dec!("0"), "You still hold some funds, you need to withdraw first.");
+
+            self.participant_minter.authorize(|| {
+                let participant_nft_manager: &ResourceManager = borrow_resource_manager!(self.participant_nft_address);
+                participant_nft_manager.burn(participant_bucket)
+            })
         }
     }
 }
---
+++ 2-oracles/auction_house/scrypto/init.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+./build.sh
+
+resim reset
+export owner_account=$(resim new-account)
+export owner=$(echo $owner_account | grep "Account component address:" | cut -d: -f2 | xargs)
+export owner_private_key=$(echo $owner_account | grep "Private key:" | cut -d: -f2 | xargs)
+
+export package=$(resim publish auction/target/wasm32-unknown-unknown/release/auction.wasm | grep "New Package" | cut -d: -f2 | xargs)
+export xrd=$(resim show $owner | grep XRD | cut -d: -f3 | cut -d, -f1 | xargs)
+
+export auction=$(resim call-function $package Auction new 1.0 10.0 1,$xrd | grep "Component:" | cut -d: -f2 | xargs)
+
+export owner_badge=$(resim show $owner | grep "Owner of" | cut -d: -f3 | cut -d, -f1 | xargs)
+
+export participant_3_account=$(resim new-account)
+export participant_3=$(echo $participant_3_account | grep "Account component address:" | cut -d: -f2 | xargs)
+export participant_3_private_key=$(echo $participant_3_account | grep "Private key:" | cut -d: -f2 | xargs)
+resim set-default-account $participant_3 $participant_3_private_key
+resim call-method $auction register
+export participant_3_badges_resource=$(resim show $participant_3 | grep "Participant of" | cut -d: -f3 | cut -d, -f1 | xargs)
+export participant_3_badge_id=$(resim show $participant_3 | grep NON_FUNGIBLE | cut -d: -f2 | cut -d, -f1 | xargs)
+
+export participant_6_account=$(resim new-account)
+export participant_6=$(echo $participant_6_account | grep "Account component address:" | cut -d: -f2 | xargs)
+export participant_6_private_key=$(echo $participant_6_account | grep "Private key:" | cut -d: -f2 | xargs)
+resim set-default-account $participant_6 $participant_6_private_key
+resim call-method $auction register
+export participant_6_badges_resource=$(resim show $participant_6 | grep "Participant of" | cut -d: -f3 | cut -d, -f1 | xargs)
+export participant_6_badge_id=$(resim show $participant_6 | grep NON_FUNGIBLE | cut -d: -f2 | cut -d, -f1 | xargs)
+
+cat <<EOT > withdraw.manifest
+CALL_METHOD ComponentAddress("$owner") "create_proof_by_amount" Decimal("1") ResourceAddress("$owner_badge");
+CALL_METHOD ComponentAddress("$auction") "withdraw";
+CALL_METHOD_WITH_ALL_RESOURCES ComponentAddress("$owner") "deposit_batch";
+EOT
+
+cat <<EOT > register.manifest
+CALL_METHOD ComponentAddress("$auction") "register";
+CALL_METHOD_WITH_ALL_RESOURCES ComponentAddress("$account") "deposit_batch";
+EOT
+
+cat <<EOT > bid_3.manifest
+CALL_METHOD ComponentAddress("$participant_3") "create_proof_by_ids" TreeSet<NonFungibleId>(NonFungibleId("$participant_3_badge_id")) ResourceAddress("$participant_3_badges_resource");
+CREATE_PROOF_FROM_AUTH_ZONE_BY_IDS TreeSet<NonFungibleId>(NonFungibleId("$participant_3_badge_id")) ResourceAddress("$participant_3_badges_resource") Proof("proof_of_participation");
+
+CALL_METHOD ComponentAddress("$participant_3") "withdraw_by_amount" Decimal("3") ResourceAddress("$xrd");
+TAKE_FROM_WORKTOP_BY_AMOUNT Decimal("3") ResourceAddress("$xrd") Bucket("bidding_bucket");
+
+CALL_METHOD ComponentAddress("$auction") "bid" Bucket("bidding_bucket") Proof("proof_of_participation");
+CALL_METHOD_WITH_ALL_RESOURCES ComponentAddress("$participant_3") "deposit_batch";
+EOT
+
+cat <<EOT > withdraw_3.manifest
+CALL_METHOD ComponentAddress("$participant_3") "create_proof_by_ids" TreeSet<NonFungibleId>(NonFungibleId("$participant_3_badge_id")) ResourceAddress("$participant_3_badges_resource");
+CREATE_PROOF_FROM_AUTH_ZONE_BY_IDS TreeSet<NonFungibleId>(NonFungibleId("$participant_3_badge_id")) ResourceAddress("$participant_3_badges_resource") Proof("proof_of_participation");
+
+CALL_METHOD ComponentAddress("$auction") "withdraw_outbidded" Proof("proof_of_participation");
+CALL_METHOD_WITH_ALL_RESOURCES ComponentAddress("$participant_3") "deposit_batch";
+EOT
+
+cat <<EOT > unregister_3.manifest
+CALL_METHOD ComponentAddress("$participant_3") "withdraw_by_ids" TreeSet<NonFungibleId>(NonFungibleId("$participant_3_badge_id")) ResourceAddress("$participant_3_badges_resource");
+TAKE_FROM_WORKTOP_BY_IDS TreeSet<NonFungibleId>(NonFungibleId("$participant_3_badge_id")) ResourceAddress("$participant_3_badges_resource") Bucket("participant_badge_bucket");
+
+CALL_METHOD ComponentAddress("$auction") "unregister" Bucket("participant_badge_bucket");
+CALL_METHOD_WITH_ALL_RESOURCES ComponentAddress("$participant_3") "deposit_batch";
+EOT
+
+cat <<EOT > bid_6.manifest
+CALL_METHOD ComponentAddress("$participant_6") "create_proof_by_ids" TreeSet<NonFungibleId>(NonFungibleId("$participant_6_badge_id")) ResourceAddress("$participant_6_badges_resource");
+CREATE_PROOF_FROM_AUTH_ZONE_BY_IDS TreeSet<NonFungibleId>(NonFungibleId("$participant_6_badge_id")) ResourceAddress("$participant_6_badges_resource") Proof("proof_of_participation");
+
+CALL_METHOD ComponentAddress("$participant_6") "withdraw_by_amount" Decimal("6") ResourceAddress("$xrd");
+TAKE_FROM_WORKTOP_BY_AMOUNT Decimal("6") ResourceAddress("$xrd") Bucket("bidding_bucket");
+
+CALL_METHOD ComponentAddress("$auction") "bid" Bucket("bidding_bucket") Proof("proof_of_participation");
+CALL_METHOD_WITH_ALL_RESOURCES ComponentAddress("$participant_6") "deposit_batch";
+EOT
+
+cat <<EOT > unregister_6.manifest
+CALL_METHOD ComponentAddress("$participant_6") "withdraw_by_ids" TreeSet<NonFungibleId>(NonFungibleId("$participant_6_badge_id")) ResourceAddress("$participant_6_badges_resource");
+TAKE_FROM_WORKTOP_BY_IDS TreeSet<NonFungibleId>(NonFungibleId("$participant_6_badge_id")) ResourceAddress("$participant_6_badges_resource") Bucket("participant_badge_bucket");
+
+CALL_METHOD ComponentAddress("$auction") "unregister" Bucket("participant_badge_bucket");
+CALL_METHOD_WITH_ALL_RESOURCES ComponentAddress("$participant_6") "deposit_batch";
+EOT
+
+resim set-default-account $participant_3 $participant_3_private_key
+resim run bid_3.manifest
+# Should fail
+echo "==================="
+echo "Begin failure case"
+echo "==================="
+resim run bid_6.manifest
+resim run unregister_3.manifest
+resim run unregister_6.manifest
+echo "==================="
+echo "End of failure case"
+echo "==================="
+
+resim set-default-account $participant_6 $participant_6_private_key
+resim run bid_6.manifest
+# Should fail
+echo "==================="
+echo "Begin failure case"
+echo "==================="
+resim run bid_3.manifest
+resim run unregister_6.manifest
+resim run unregister_3.manifest
+echo "==================="
+echo "End of failure case"
+echo "==================="
+resim run bid_6.manifest
+resim run unregister_6.manifest
+echo "==================="
+echo "Begin failure case"
+echo "==================="
+resim run bid_3.manifest
+resim run unregister_6.manifest
+resim run unregister_3.manifest
+echo "==================="
+echo "End of failure case"
+echo "==================="
+
+resim set-default-account $participant_3 $participant_3_private_key
+resim run withdraw_3.manifest
+resim run unregister_3.manifest
+echo "==================="
+echo "Begin failure case"
+echo "==================="
+resim run bid_3.manifest
+resim run unregister_6.manifest
+resim run unregister_3.manifest
+echo "==================="
+echo "End of failure case"
+echo "==================="
+
+resim set-default-account $owner $owner_private_key
+
+resim run withdraw.manifest
